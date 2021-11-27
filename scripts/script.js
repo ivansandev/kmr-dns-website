ad0abd88848b985abe8c87e961338e42e4a770cb
Ivan Sandev <ivansandev@telco.com>
2021-11-27 02:14:16 +0100

    author field added

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,19 @@
 function togglePopup() {
-    document.getElementById("popup-1").classList.toggle("active");
+    document.getElementById("popup-1").classList.toggle("active")
 }
+
+let author = document.getElementById("last-wrapper")
+let authorButton = document.getElementById("last")
+let authorField = document.getElementById("author-quick-info")
+
+authorButton.addEventListener("mouseover", (event) => {
+    event.target.style.backgroundColor = "#D0D2D3"
+    authorField.style.opacity = "1"
+}, false)
+
+author.addEventListener("mouseleave", (event) => {
+    setTimeout(() => {
+        authorButton.style.backgroundColor = ""
+        authorField.style.opacity = ""
+    }, 300)
+}, false)
